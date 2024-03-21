9ea4aff1183e63f5171b878610e5c3f94b0c622c
unknown <0210superman@163.com>
2024-03-21 16:24:28 +0800

    cut slice

--- codes/stlSlice.py
+++ codes/stlSlice.py
@@ -4,10 +4,23 @@ Created on Thu Mar 21 15:28:20 2024
 
 @author: Admin
 """
-
-import numpy as np
 import cv2
-import stl
-from stl import mesh
+import meshio
+from mayavi import mlab
+import numpy as np
 
-MESH = mesh.Mesh.from_file('C:\AAAWeichen\Mold (important!)\AM Mould Insert (GOM scan) (2)\Mould insert_F_standard.stl')
+# from stl import mesh
+filename = 'C:\AAAWeichen\Mold (important!)\AM Mould Insert (GOM scan) (2)\Mould insert_F_standard.stl'
+# model = np.array(mesh.Mesh.from_file(filename).vectors)
+mesh = meshio.read(filename)
+
+# Get vertices and faces from the mesh
+vertices = mesh.points
+faces = mesh.cells_dict['triangle']
+
+
+# Plot using Mayavi
+fig = mlab.figure()
+mlab.triangular_mesh(vertices[:, 0], vertices[:, 1], vertices[:, 2], faces)
+mlab.show()
+
